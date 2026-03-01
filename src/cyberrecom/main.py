9e12f7132dac61b152fd7c95b63c9fa0a54fcb04
Sergio Gutierrez Fernandez <sergio.gutierrez@alumnos.upm.es>
2026-03-01 18:21:29 +0100

    feat: implement initialize_simulation_data function to structure simulation results with metadata and threat details into a json

--- src/cyberrecom/main.py
+++ src/cyberrecom/main.py
@@ -20,27 +20,6 @@ EXCEL_PATH = Path(__file__).parent.parent.parent / "data" / "asset_catalog_valid
 CPDS = id_test.read_constants()
 
 
-#===============================[JSON FUNCTIONS]===========================================#
-def initialize_simulation_data(threat_vector: dict) -> dict:
-    """Inicializa la estructura de datos para almacenar resultados de simulación"""
-    
-    return {
-        "metadata": {
-            "timestamp": datetime.now().isoformat(),
-            "version": "1.0.0",
-            "threat_ttp_id": threat_vector['ttp_id'],
-            "threat_confidence": float(threat_vector['confidence']),
-            "initial_asset": threat_vector['asset'],
-            "tactic": threat_vector['tactic'],
-            "db_path": str(DB_PATH),
-            "excel_source": str(EXCEL_PATH)
-        },
-        "threat_vector": threat_vector,
-        "affected_nodes": {},
-        "affected_edges": {},
-        "levels_analysis": {}
-    }
-
 #==============================[MAIN FUNCTION]===========================================#
 
 def main() -> None:
